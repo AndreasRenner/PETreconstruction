7dc7618062d01b56e8ed9fd4df5189e1327dfce0
Andreas Renner <AndreasRenner@gmx.at>
2016-11-21 21:12:00 +0100

    Added option to output detailed information about all Tags

--- build_static_mMR.m
+++ build_static_mMR.m
@@ -14,32 +14,88 @@ sinoDim=Nbins*Nproj*Nplanes;
 %cd('/home/andreas/data/PET_raw_data_20160603');
 filesize=dir(filename);
 Ncs=ceil(filesize.bytes/4);
-fprintf('Estimated Number of Tags: %s\r', num2str(Ncs));
+fprintf('Estimated Number of Tags: \t %10.0f\r', Ncs);
 
 fid=fopen(filename,'r');
 dlist=fread(fid,[Ncs],'uint32');    
 fclose(fid);
 %cd('/home/andreas/code/PETreconstruction');
-%LMwordcount=487595518;
-%header=Ncs-LMwordcount;
-%disp(header);
+
+% -------------------------------------------------------
+% Option 0: Output detailed information about all Tags
+if options==0
+    % Event Packets:  0XXX ...
+    prompts    = 0; % 01XX ...
+    delays     = 0; % 00XX ...
+    % Tag Packets:    1XXX ...
+    timeTag    = 0; % 100X ...
+    DtimeTag   = 0; % 101X ...
+    motionTag  = 0; % 1100 ...
+    patientTag = 0; % 1110 ...
+    controlTag = 0; % 1111 ...
+    currentTime= 0;
+    for i=1:length(dlist)
+        if     dlist(i) < 1073741824
+            delays = delays + 1;
+            %fprintf('Pos. %u\tat %s ms:\tDelay Event:\t%s\r',i,currentTime,num2str(dec2bin(dlist(i))));
+        elseif dlist(i) < 2147483648
+            prompts = prompts + 1;
+            %fprintf('Pos. %u\tat %s ms:\tPrompt Event:\t%s\r',i,currentTime,num2str(dec2bin(dlist(i))));
+        elseif dlist(i) < 2684354560
+            timeTag = timeTag + 1;
+            currentTime=num2str(dlist(i)-2^31);
+            %fprintf('Pos. %u\tat %s ms:\tTime Tag:\t%s\r',i,currentTime,num2str(dec2bin(dlist(i))));
+        elseif dlist(i) < 3221225472
+            DtimeTag = DtimeTag + 1;
+            %fprintf('Pos. %u\tat %s ms:\tDead-Time Tag:\t%s\r',i,currentTime,num2str(dec2bin(dlist(i))));
+        elseif dlist(i) < 3758096384
+            motionTag = motionTag + 1;
+            %fprintf('Pos. %u\tat %s ms:\Motion Tag:\t%s\r',i,currentTime,num2str(dec2bin(dlist(i))));
+        elseif dlist(i) < 4026531840
+            patientTag = patientTag + 1;
+            if     dlist(i)==3774877696
+                fprintf('Pos. %u\tat %s ms:\tPatient Tag - Respiratory Trigger Gate on\r',i,currentTime);
+            elseif dlist(i)==3774877697
+                fprintf('Pos. %u\tat %s ms:\tPatient Tag - Respiratory Trigger Gate off\r',i,currentTime);
+            end
+            %fprintf('Pos. %u\tat %s ms:\tPatient Tag:\t%s\r',i,currentTime,num2str(dec2bin(dlist(i))));
+        else
+            controlTag = controlTag + 1;
+            if     dlist(i)==4294901760
+                fprintf('Pos. %u\tat %s ms:\tControl Tag - Start of Acquisition\r',i,currentTime);
+            elseif dlist(i)==4286545920
+                fprintf('Pos. %u\tat %s ms:\tControl Tag - Time Synchronization with MR\r',i,currentTime);
+            end
+            %fprintf('Pos. %u\tat %s ms:\tControl Tag:\t%s\r',i,currentTime,num2str(dec2bin(dlist(i))));     
+        end
+    end
+    events = prompts + delays;
+    tags   = timeTag + DtimeTag + motionTag + patientTag + controlTag;
+    fprintf('Total number of Event Packets: \t %10.0f\r', events);
+    fprintf('Total number of Tag Packets:   \t %10.0f\r\n', tags);
+    fprintf('Prompt Events:  \t %10.0f\r', prompts);
+    fprintf('Delay Events:   \t %10.0f\r', delays);
+    fprintf('Time Tags:      \t %10.0f\r', timeTag);
+    fprintf('Dead-Time Tags: \t %10.0f\r', DtimeTag);
+    fprintf('Motion Tags:    \t %10.0f\r', motionTag);
+    fprintf('Patient Tags:   \t %10.0f\r', patientTag);
+    fprintf('Control Tags:   \t %10.0f\r', controlTag);
 
 % -------------------------------------------------------
 % Option 1: Output prompts per second
-if options==1
-    ptag=[];
-    Ttag=[];
-    p=1;
-    T=1;
+elseif options==1
+    p=0;
+    T=0;
     tmplen=0;
     for i=1:length(dlist)
         if (dlist(i)<(2^31))&&(dlist(i)>=(2^30))
-            ptag(p)=dlist(i);
             p=p+1;
         elseif (dlist(i)>=(2^31))&&(dlist(i)<2684354560)
-            Ttag(T)=dlist(i);
-            fprintf('Prompts in ms %u: %u\r', (T-1), (length(ptag)-tmplen));
-            tmplen=length(ptag);
+            Ttag=dlist(i);
+            if mod(Ttag-(2^31),1000)==0
+                fprintf('Prompts in s %u: %u\r', T/1000, (p-tmplen));
+                tmplen=p;
+            end
             T=T+1;
         end
     end
@@ -47,21 +103,16 @@ if options==1
 % -------------------------------------------------------
 % Option 2: Output detailed dead-time information
 elseif options==2
-    Dtag=dlist(find((dlist>=2684354560)&(dlist<3221225472)));
-    %DeadTime=zeros(length(Dtag));
-    D=1;
-    Ttag=[];
+    D=0;
     LostEventsFirstLossyNode=0;
     LostEventsSecondLossyNode=0;
     millionEvents=0;
     for i=1:length(dlist)
         if (dlist(i)>=(2^31))&&(dlist(i)<2684354560)
             % Time Tag
-            Ttag=dlist(i);
-            Ttag=num2str(Ttag-2^31);
+            Ttag=num2str(dlist(i)-2^31);
         elseif (dlist(i)>=2684354560)&&(dlist(i)<3221225472)
             % Dead-Time Tag
-            %DeadTime(D)=dlist(i);
             D=D+1;
             binaryTag=num2str(dec2bin(dlist(i)));
             typefield=bin2dec(binaryTag(4:6));
@@ -75,20 +126,21 @@ elseif options==2
                 LostEventsSecondLossyNode=LostEventsSecondLossyNode+loss;
                 millionEvents=millionEvents+1;
                 fprintf('2. Lossy Node: %u \t lost events at %s [ms]\r', loss, Ttag);
-            else
-                blocknum=bin2dec(binaryTag(4:13));
-                singles=bin2dec(binaryTag(14:32));
-                fprintf('Block: %u \tEvents: %u \t Time[ms]: %s\r', blocknum, singles, Ttag);
+            %else
+                %blocknum=bin2dec(binaryTag(4:13));
+                %singles=bin2dec(binaryTag(14:32));
+                %fprintf('Block: %u \tEvents: %u \t Time[ms]: %s\r', blocknum, singles, Ttag);
             end
         end
     end
     totalLoss=LostEventsSecondLossyNode+LostEventsFirstLossyNode;
-    totalEvents=millionEvents*1048575;
-    fprintf('Total Dead-time marks: %s\r',num2str(length(Dtag)));
+    totalEvents=(millionEvents-1)*1000000;%1048575;
+    fprintf('Total Dead-time marks: %u\r',D);
     fprintf('Number of lost events inserted by the "1. Lossy Node": %u\r', LostEventsFirstLossyNode);
     fprintf('Number of lost events inserted by the "2. Lossy Node": %u\r', LostEventsSecondLossyNode);
     fprintf('Total number of lost event packets: \t %u\r', totalLoss);
-    fprintf('Total number of initial events: \t %u\r', totalEvents);
+    fprintf('Total number of initial events: \t~%u\r', totalEvents);
+    fprintf('Estimated Number of Tags: \t %u\r', Ncs);
     
 % -------------------------------------------------------
 % Option 3: Create Sinograms of the whole acquisition
@@ -98,15 +150,8 @@ elseif options==3
     % Randoms lower or equal [001111...1]
     rtag = dlist(find((dlist<2^30)));
 
-    % TAGs (Time marks, dead-time tracking, fisio marks)
-    Ttag = dlist(find((dlist>=2^31)&(dlist<2684354560)));
-    Dtag = dlist(find((dlist>=2684354560)&(dlist<3221225472))); %>=101 0..0 <110 0..0
-    Ftag = dlist(find((dlist>=3758096384)&(dlist<3825205248))); %>=111 0..0 <1110 010..0
-
     % remove preceding tag if necessary
     ptag = ptag-(2^30);
-    %Ttag = Ttag-(2^31);   % returns time in ms
-    %find(~mod(Ttag, 1000))     % returns time in s
     % clear out-of-sinograms indices
     ptag = ptag((ptag<=sinoDim)&(ptag>0));
     rtag = rtag((rtag<=sinoDim)&(rtag>0));    
@@ -123,15 +168,7 @@ elseif options==3
     % Output for user
     fprintf('Finished reading list file\r\n');
     fprintf('Prompts\t\t:\t%s\r',num2str(length(ptag)));
-    fprintf('Randoms\t\t:\t%s\r',num2str(length(rtag)));  
-    time=length(Ttag)*paso_t;
-    Ntags=length(ptag)+length(rtag)+length(Ttag)+length(Dtag)+length(Ftag);
-    fprintf('ACQ time [s]\t:\t%s\r',num2str(time));
-    fprintf('Total Number of Tags \t:\t%s\r\n',num2str(Ntags));
-    if(Ntags<Ncs);
-        %fprintf('Total number of Tags is smaller than "Ncs"!\n');
-        fprintf('All Tags of the file were considered!\n');
-    end
+    fprintf('Randoms\t\t:\t%s\r',num2str(length(rtag)));
     
 % -------------------------------------------------------
 % Option 4: Cut the first X and the last Y ms of the acquisition
@@ -158,8 +195,15 @@ else
 
     % TAGs (Time marks, dead-time tracking, fisio marks)
     Ttag = cutdlist(find((cutdlist>=2^31)&(cutdlist<2684354560)));
+    Ttags=length(Ttag);
+    time =Ttags*paso_t;
+    clear Ttag;
     Dtag = cutdlist(find((cutdlist>=2684354560)&(cutdlist<3221225472)));
+    Dtags=length(Dtag);
+    clear Dtag;
     Ftag = cutdlist(find((cutdlist>=3758096384)&(cutdlist<3825205248)));
+    Ftags=length(Ftag);
+    clear Ftag;
 
     % remove preceding tag - necessary for ptag only, as the preceding
     % tag of rtag is already 0
@@ -171,7 +215,13 @@ else
 
     % build sinograms
     sino=accumarray(ptag,1,[sinoDim,1])-accumarray(rtag,1,[sinoDim,1]);
-
+    
+    ptags = length(ptag);
+    rtags = length(rtag);
+    Ntags = ptags + rtags + Ttags + Dtags + Ftags;
+    clear ptag;
+    clear rtag;
+    
     % write sinograms to file
     sinogramname = strcat('sinogram_static_', filename, '_cut.raw');
     fid=fopen(sinogramname,'w');
@@ -180,29 +230,10 @@ else
 
     % Output for user
     fprintf('Finished reading list file\r\n');
-    fprintf('Prompts\t\t:\t%s\r',num2str(length(ptag)));
-    fprintf('Randoms\t\t:\t%s\r',num2str(length(rtag)));  
-    time=length(Ttag)*paso_t;
-    Ntags=length(ptag)+length(rtag)+length(Ttag)+length(Dtag)+length(Ftag);
+    fprintf('Prompts\t\t:\t%s\r',num2str(ptags));
+    fprintf('Randoms\t\t:\t%s\r',num2str(rtags));
     fprintf('ACQ time [s]\t:\t%s\r',num2str(time));
     fprintf('Total Number of Tags \t:\t%s\r\n',num2str(Ntags));
-    % Output lost events
-    DeadTime=zeros(length(Dtag));
-    D=1;
-    LostEvents=0;
-    for i=1:length(cutdlist)
-        if (cutdlist(i)>=2684354560)&&(cutdlist(i)<3221225472)
-            DeadTime(D)=cutdlist(i);
-            D=D+1;
-            binaryTag=num2str(dec2bin(cutdlist(i)));
-            blocknum=bin2dec(binaryTag(4:13));
-            singles=bin2dec(binaryTag(14:32));
-            if blocknum==896||blocknum==768
-                LostEvents=LostEvents+singles;
-            end
-        end
-    end
-    fprintf('Total number of lost events: %u\r\n', LostEvents);
     
     if(Ntags<Ncs);
         %fprintf('Total number of Tags is smaller than "Ncs"!\n');
