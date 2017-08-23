6f06118e109c8635f7f6c660a8ddfda73daade37
Andreas Renner <AndreasRenner@gmx.at>
2017-08-23 11:40:17 +0200

    Added a method to check for nan in matrix; added a method to resize the image matrix; added a method to multiply the slices to account for the axial sensitivity change of the PET.

--- reconSino.m
+++ reconSino.m
@@ -11,8 +11,34 @@ SSRBSino = zeros(Nbins,Nproj,Nslices,'double');
 fid  = fopen(filename,'r');
 for i=1:Nslices
   Sino2D = fread(fid,[Nbins,Nproj],'float32');
-  SIN2D  = double(Sino2D);
+  SIN2D = double(Sino2D);
+  
+  %for v=1:Nbins
+  %  for w=1:Nproj
+  %    if isnan(Sino2D(v,w))
+  %      SIN2D(v,w) = 0.0;
+  %    end
+  %  end
+  %end
+
+  %SIN2D = imresize(Sino2D,2,'nearest');
+  %for v=1:Nbins
+  %  for w=1:Nproj
+  %    if Sino2D(v,w)==0
+  %      SIN2D(v,w) = 0.0;
+  %    else
+  %      SIN2D(v,w) = 5*log(Sino2D(v,w));
+  %    end
+  %  end
+  %end
   SSRBSino(:,:,i) = SSRBSino(:,:,i) + SIN2D;
+  %if i<50
+  %  SSRBSino(:,:,i) = SSRBSino(:,:,i) + SIN2D/(0.1020*i+1.9365);
+  %elseif i<76
+  %  SSRBSino(:,:,i) = SSRBSino(:,:,i) + SIN2D/7;
+  %else
+  %  SSRBSino(:,:,i) = SSRBSino(:,:,i) + SIN2D/(-0.1020*i+14.6531);
+  %end
 end
 fclose(fid);
 
