9e01815f6fc8c53c9572572304b119a04255271d
Andreas Renner <AndreasRenner@gmx.at>
2017-10-04 17:17:24 +0200

    Added method to directly consider the mask for the ratio; added choice which reconstruction method is wanted

--- reconSinoRatio.m
+++ reconSinoRatio.m
@@ -1,9 +1,9 @@
-function reconSinoRatio(nameBlank,nameTrans,reconMethod)
+function reconSinoRatio(nameBlank,nameTrans,nameMask,reconMethod)
 % Reconstruct ratio between Blanc- and Transmission-Scan
 
-filenameBlank = strcat('SSRB_complete_',  nameBlank,'.raw');
-filenameTrans = strcat('SSRB_complete_',  nameTrans,'.raw');
-filenameMask  = strcat('mask_SSRB_ratio_',nameTrans,'.raw');
+filenameBlank = strcat('SSRB_complete_',nameBlank,'.raw');
+filenameTrans = strcat('SSRB_complete_',nameTrans,'.raw');
+filenameMask  = strcat('SSRB_',nameMask,nameTrans,'.raw');
 
 % Basic Parameters of Siemens Biograph mMR
 Nbins   = 344;         % Number of radial bins (NRAD)
@@ -19,21 +19,16 @@ Nslices = 127;         % Number of slices (2*Nrings -1)
 %load('SSRB_Trans.mat');
 SinoBlank = zeros(Nbins,Nproj,Nslices,'double');
 SinoTrans = zeros(Nbins,Nproj,Nslices,'double');
-SinoMask  = zeros(Nbins,Nproj,Nslices,'double');
 fid1      = fopen(filenameBlank,'r');
 fid2      = fopen(filenameTrans,'r');
-fid3      = fopen(filenameMask, 'r');
 for i=1:Nslices
   Blank2D = fread(fid1,[Nbins,Nproj],'float32');
   Trans2D = fread(fid2,[Nbins,Nproj],'float32');
-  Mask2D  = fread(fid3,[Nbins,Nproj],'float32');
   SinoBlank(:,:,i) = SinoBlank(:,:,i) + Blank2D;
   SinoTrans(:,:,i) = SinoTrans(:,:,i) + Trans2D;
-  SinoMask(:,:,i)  = SinoMask(:,:,i)  + Mask2D;
 end
 fclose(fid1);
 fclose(fid2);
-fclose(fid3);
 
 % KernelSize should be 2*ceil(2*sigma)+1
 %SinoBlank = smooth3(SinoBlank,'gaussian',[9 9 7],2);
@@ -65,21 +60,23 @@ for u=1:Nbins
 end
 
 %SinoRatio=smooth3(SinoRatio,'gaussian',[9 9 7],2);
-SinoRatio=smooth3(SinoRatio,'gaussian',[3 3 3],0.42466);
 
-name = strcat('SSRB_333ratio333_',nameTrans,'.raw');
+name = strcat('SSRB_333ratio_',nameTrans,'.raw');
 fid = fopen(name,'w');
 fwrite(fid,SinoRatio,'float32');
 fclose(fid);
 
-% ****************************
-% ToDo:
-% multiplication with mask
-% ****************************
+SinoRatio = FillSinoGaps(name,filenameMask);
+SinoRatio = smooth3(SinoRatio,'gaussian',[3 3 3],0.42466);
+
+name = strcat('SSRB_333ratio333_SEG_',nameTrans,'.raw');
+fid = fopen(name,'w');
+fwrite(fid,SinoRatio,'float32');
+fclose(fid);
 
 if strcmp(reconMethod, 'FBP')
   theta = 180./Nproj;
-  for i=1:Nslices
+  parfor i=1:Nslices
     recon(:,:,i) = 4.83559*iradon(SinoRatio(:,:,i),theta,Nbins);
   end
   name = strcat('recon_333ratio333_',nameTrans,'.raw');
