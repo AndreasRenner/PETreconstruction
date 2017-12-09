7f44dadf31aef04f504a309701134110fe994902
Andreas Renner <AndreasRenner@gmx.at>
2017-12-09 15:53:03 +0100

    Adapted for new main.m

--- FillSinoGaps.m
+++ FillSinoGaps.m
@@ -10,11 +10,15 @@ Sino = zeros(Nbins,Nproj,Nslices,'double');
 Mask = zeros(Nbins,Nproj,Nslices,'double');
 %recon= zeros(Nbins,Nbins,Nslices,'double');
 
+SE = strel('disk',30);
+
 fid1 = fopen(filename,'r');
 fid2 = fopen(maskname,'r');
 for i=1:Nslices
   Sino2D = fread(fid1,[Nbins,Nproj],'float32');
   Mask2D = fread(fid2,[Nbins,Nproj],'float32');
+  Mask2D = imdilate(Mask2D,SE);
+  Mask2D = imgaussfilt(Mask2D,10);
   for j=1:Nproj
     for k=1:Nbins
       if Mask2D(k,j) && Sino2D(k,j)<=0
