a11b0acea673a86b748af573e93978f13d7e51b7
Andreas Renner <AndreasRenner@gmx.at>
2018-04-10 11:55:54 +0200

    added method to read the emission sinogram from multiple files

--- main.m
+++ main.m
@@ -20,9 +20,21 @@ if     strcmp(filenameB,'05BlankFast')
   faktor   = [1.05,1.05,1.05,1,0.98,0.9,1.08,0.9,1,1];
 elseif strcmp(filenameB,'01Blank')
   decayCor = 930;
-  NscansB  = 16;
+  NscansB  = 16; % last Scan is not comlete
   faktor   = [1.16,1.14,1.11,1.08,1.07,1.05,1.06,1.01,1.02,1,0.94,0.92,0.92,0.9,0.88];
   direction= 1;
+elseif strcmp(filenameB,'blankPig1')
+  decayCor = 0;
+  NscansB  = 22; % last Scan is not complete
+  faktor   = [1.14,1.14,1.12,1.09,1.08,1.08,1.06,1.08,1.03,1.03, ...
+      1.03,1,1,0.98,0.96,0.96,0.94,0.96,0.92,0.93,0.9,1,1];
+  direction= 0;
+elseif strcmp(filenameB,'blankPig2')
+  decayCor = 0;
+  NscansB  = 22;
+  faktor   = [1.11,1.12,1.1,1.07,1.08,1.04,1.03,1.03,1.04,1,1, ...
+      0.99,0.99,0.98,0.97,0.94,0.94,0.93,0.92,0.93,0.9,1,1];
+  direction= 0;
 end
 
 %% Allocate memory for Blank acquisition
@@ -35,7 +47,7 @@ tstopB     = zeros(Nparts,NscansB);
 % For first Scan do not perform adaption of position
 % -> first Scan is used as reference
 [sino,indexref,tstart,tstop] = readScan(filenameB,1,NscansB,Nparts, ...
-    faktor(1),0,direction,0,0);
+    faktor(1),0,direction,0,0,0);
 
 %% DECAY CORRECTION
 durrationRef = tstop-tstart;
@@ -47,15 +59,17 @@ end
 % is used as reference and a constant value for all parts
 
 %% Save files for visual check
-%for i=1:Nparts
-%  name = strcat('Scan1_',num2str(i));
-%  SSRBSino(:,:,:)=sino(i,:,:,:);
-%  newName = strcat('sino_SSRB_', name, '.raw');
-%  fid = fopen(newName,'w');
-%  fwrite(fid,SSRBSino,'float32');
-%  fclose(fid);
-%  clear SSRBSino;
-%end
+cd('/media/andreas/CorneaOCT_StudyData2/Backup/Leipzig/Evaluation/ScansBlankPig1/sinoScan1');
+for i=1:Nparts
+  name = strcat('Scan1_',num2str(i));
+  SSRBSino(:,:,:)=sino(i,:,:,:);
+  newName = strcat('sino_SSRB_', name, '.raw');
+  fid = fopen(newName,'w');
+  fwrite(fid,SSRBSino,'float32');
+  fclose(fid);
+  clear SSRBSino;
+end
+cd('/home/andreas/code/PETreconstruction');
 
 %%
 sinoTotalB = sinoTotalB + sino;
@@ -78,7 +92,7 @@ for i=2:(NscansB-1)
     direction = 1;
   end
   [sino,~,tstart,tstop] = readScan(filenameB,i,NscansB,Nparts, ...
-      faktor(i),kumFaktor,direction,line1and2,indexref);
+      faktor(i),kumFaktor,direction,dirRef,line1and2,indexref);
   
   %% DECAY CORRECTION + SCAN-TIME CORRECTION
   durration = tstop-tstart;
@@ -89,6 +103,12 @@ for i=2:(NscansB-1)
   end
   
   %% Save files for visual check
+  pathname = strcat('/media/andreas/CorneaOCT_StudyData2/Backup/Leipzig/Evaluation/ScansBlankPig1/sinoScan',num2str(i));
+  try
+    cd(pathname);
+  catch ME
+    ME;
+  end
   for j=1:Nparts
     name = strcat('Scan',num2str(i),'_',num2str(j));
     SSRBSino(:,:,:)=sino(j,:,:,:);
@@ -98,6 +118,7 @@ for i=2:(NscansB-1)
     fclose(fid);
     clear SSRBSino;
   end
+  cd('/home/andreas/code/PETreconstruction');
   
   %% add new sinograms to total sinogram
   sinoTotalB = sinoTotalB + sino;
@@ -107,6 +128,31 @@ for i=2:(NscansB-1)
   %%
   kumFaktor = kumFaktor + faktor(i);
 end
+
+%% Read total Sinogram of Blank Scan from single Scan files
+sinoTotalB = zeros(Nparts,Nbins,Nproj,Nslices);
+cd('/media/andreas/CorneaOCT_StudyData2/Backup/Leipzig/Evaluation/ScansBlankPig2');
+for i=1:NscansB
+  name = strcat('sinoScan',num2str(i));
+  cd(name)
+  sinoTotalTmp = zeros(Nparts,Nbins,Nproj,Nslices);
+  for j=1:Nparts
+    sinoname = strcat('sino_SSRB_Scan',num2str(i),'_',num2str(j),'.raw');
+    SSRBSino = zeros(Nbins,Nproj,Nslices,'double');
+    fid = fopen(sinoname,'r');
+    for l=1:Nslices
+      Sino2D = fread(fid,[Nbins,Nproj],'float32');
+      SIN2D = double(Sino2D);
+      SSRBSino(:,:,l) = SSRBSino(:,:,l) + SIN2D;
+    end
+    fclose(fid);
+    sinoTotalTmp(j,:,:,:) = SSRBSino(:,:,:);
+  end
+  sinoTotalB = sinoTotalB + sinoTotalTmp;
+  cd ..
+end
+clear sinoTotalTmp;
+cd('/home/andreas/code/PETreconstruction');
 
 %% Correct for number of scans
 sinoTotalB = sinoTotalB./i;
@@ -123,6 +169,7 @@ for j=1:Nparts
 end
 
 %% Read total Sinogram of Blank Scan from file
+sinoTotalB = zeros(Nparts,Nbins,Nproj,Nslices);
 for j=1:Nparts
   SSRBSino = zeros(Nbins,Nproj,Nslices,'double');
   name = strcat(filenameB,'_',num2str(j));
@@ -159,11 +206,21 @@ elseif strcmp(filenameT, '03TransPhantom1')
   decayCor = 3668;
   NscansT  = 7;
   faktor   = [1.05,1.05,1.02,1,0.98,0.96,1];
-  direction=1;
+  direction= 1;
 elseif strcmp(filenameT, '04TransPhantom2HOT')
   decayCor = 6231;
   NscansT  = 7;
   faktor   = [0.9,0.9,1.5,1.42,0.82,0.75,1];
+  direction= 0;
+elseif strcmp(filenameT, 'transPig1')
+  decayCor = 3193;
+  NscansT  = 12;
+  faktor   = [1.2,1.1,1.1,1,1,1,1,0.98,0.92,0.88,0.9,1,1];
+  direction= 0;
+elseif strcmp(filenameT, 'transPig2')
+  decayCor = 3672;
+  NscansT  = 12;
+  faktor   = [1.1,1.11,1.04,1.06,1.02,1,0.98,0.96,0.94,0.92,0.9,1,1];
   direction= 0;
 end
 
@@ -178,7 +235,7 @@ kumFaktor = 0;
 for i=1:NscansT
     
   [sino,~,tstart,tstop] = readScan(filenameT,i,NscansT,Nparts, ...
-      faktor(i),kumFaktor,direction,line1and2,indexref);
+      faktor(i),kumFaktor,direction,dirRef,line1and2,indexref);
   
   if direction
     direction = 0;
@@ -194,16 +251,16 @@ for i=1:NscansT
     sino(j,:,:,:) = sino(j,:,:,:)/decayF(j)*scanTcor(j);
   end
   
-  %%% Save files for visual check
-  %for j=1:Nparts
-  %  name = strcat('ScanTrans',num2str(i),'_',num2str(j));
-  %  SSRBSino(:,:,:)=sino(j,:,:,:);
-  %  newName = strcat('sino_SSRB_', name, '.raw');
-  %  fid = fopen(newName,'w');
-  %  fwrite(fid,SSRBSino,'float32');
-  %  fclose(fid);
-  %  clear SSRBSino;
-  %end
+  %% Save files for visual check
+  for j=1:Nparts
+    name = strcat('ScanTrans',num2str(i),'_',num2str(j));
+    SSRBSino(:,:,:)=sino(j,:,:,:);
+    newName = strcat('sino_SSRB_', name, '.raw');
+    fid = fopen(newName,'w');
+    fwrite(fid,SSRBSino,'float32');
+    fclose(fid);
+    clear SSRBSino;
+  end
   
   %% add new sinograms to total sinogram
   sinoTotalT = sinoTotalT + sino;
@@ -230,6 +287,7 @@ for j=1:Nparts
 end
 
 %% Read total Sinogram of Trans Scan from file
+sinoTotalT = zeros(Nparts,Nbins,Nproj,Nslices);
 for j=1:Nparts
   SSRBSino = zeros(Nbins,Nproj,Nslices,'double');
   name = strcat(filenameT,'_',num2str(j));
@@ -244,7 +302,37 @@ for j=1:Nparts
   sinoTotalT(j,:,:,:) = SSRBSino(:,:,:);
 end
 
-%% Read Emission Data
+%% Read Emission Data - Multiple Emission Sinograms
+NpartE  = 13;
+%tstart  = [ 0,141,283,423,563,703,844, 982,1123,1263,1406,1546,1688]; %TransPig2
+%tstop   = [64,205,347,487,627,767,908,1046,1187,1328,1470,1610,1799];
+tstart  = [0,146,299,444,591,733,876,1021,1162,1302,1443,1586,1737]; %TransPig1
+tstop   =[63,209,362,507,654,796,940,1084,1225,1365,1507,1649,1799];
+acqtime = tstop-tstart; %for each sinogram
+%
+sinoEm  = zeros(Nbins,Nproj,NSliTot);
+for i=1:NpartE
+  % (1) DECAY CORRECTION
+  decayF(i) = 2^(-(decayCor+tstart(i)+acqtime(i)/2)/halflifeFDG);
+  % (2) SCAN-TIME CORRECTION
+  % meantime could be replaced by the actual time of each
+  % transmission scan
+  meantime = 0.131; % BlankPig1
+  %meantime = 0.131; % BlankPig2
+  timeF(i) = meantime/acqtime(i);
+  % (3) SCAN-NUMBER CORRECTION
+  totalCorF(i) = timeF(i)/decayF(i)/NpartE;
+  nameEm = strcat('sino_SSRB_',filenameT,num2str(i),'emission.raw');
+  fid = fopen(nameEm,'r');
+  for j=1:NSliTot
+    sinoEm2D = fread(fid,[Nbins,Nproj],'float32');
+    SIN2D = double(sinoEm2D)*totalCorF(i);
+    sinoEm(:,:,j) = sinoEm(:,:,j) + SIN2D;
+  end
+  fclose(fid);
+end
+
+%% Read Emission Data - Single Emission Sinogram
 tstart = 311.96;
 acqtime= 317.977;
 % (1) DECAY CORRECTION
@@ -252,7 +340,8 @@ decayF   = 2^(-(decayCor+tstart+acqtime/2)/halflifeFDG);
 % (2) SCAN-TIME CORRECTION
 % meantime could be replaced by the actual time of each
 % transmission scan
-meantime = 0.16;%*NscansT;
+%meantime = 0.16; % 01Blank
+meantime = 0.131; % BlankPig2
 timeF    = meantime/acqtime;
 % (3) SCAN-NUMBER CORRECTION is not necessary (counts are already
 %     divided by number of scans)
@@ -267,37 +356,8 @@ for i=1:NSliTot
 end
 fclose(fid);
 
-%% Substract emission contamination from transmission window
-% for each part get indexref and substract emission sinogram from
-% transmission data
-%faktor = ones(NSliTot,1);
-% make faktor 2 for i = 63 and 64
-%for i=8:63
-%  faktor(i) = (i/56 + 49/56);
-%  faktor(NSliTot-i) = faktor(i);
-%end
-% make faktor 1.8 for i = 63 and 64
-%for i=8:63
-%  faktor(i) = (i*0.8/56 + 50.4/56);
-%  faktor(NSliTot-i) = faktor(i);
-%end
-
-%for j=1:Nparts
-%  % get current part
-%  currentTrans(:,:,:) = sinoTotalT(j,:,:,:);
-%  l = 1;
-%  for i=(indexref(j)-6):(indexref(j)+6)
-%    if i>0 && i<=NSliTot
-%      % substract emission sinogram from current part
-%      currentTrans(:,:,l) = currentTrans(:,:,l)-sinoEm(:,:,i);%faktor(i);
-%    end
-%    l = l+1;
-%  end
-%  % save modified current part
-%  sinoTotalT(j,:,:,:) = currentTrans(:,:,:);
-%end
-
 %% make sino Total Emission
+% this is needed for scatter reduction using line1and2
 sinoTotalE = zeros(Nparts,Nbins,Nproj,Nslices);
 currentEmiss = zeros(Nbins,Nproj,Nslices);
 for j=1:Nparts
@@ -690,21 +750,21 @@ toc
 %% make complete sinogram using indexref
 BlankComplete = zeros(Nbins,Nproj,NSliTot);
 TransComplete = zeros(Nbins,Nproj,NSliTot);
-%EmissComplete = zeros(Nbins,Nproj,NSliTot);
-MaskComplete  = zeros(Nbins,Nproj,NSliTot);
+EmissComplete = zeros(Nbins,Nproj,NSliTot);
+%MaskComplete  = zeros(Nbins,Nproj,NSliTot);
 
 for j=1:Nparts
   l = 1;
   sinoAddBlank(:,:,:) = sinoTotalB(j,:,:,:);
   sinoAddTrans(:,:,:) = sinoTotalT(j,:,:,:);
-  %sinoAddEmiss(:,:,:) = sinoTotalE(j,:,:,:);
-  sinoAddMask (:,:,:) = testMask  (j,:,:,:);
+  sinoAddEmiss(:,:,:) = sinoTotalE(j,:,:,:);
+%  sinoAddMask (:,:,:) = testMask  (j,:,:,:);
   for i=(indexref(j)-6):(indexref(j)+6)
     if i>0 && i<=NSliTot
       BlankComplete(:,:,i)=BlankComplete(:,:,i)+sinoAddBlank(:,:,l);
       TransComplete(:,:,i)=TransComplete(:,:,i)+sinoAddTrans(:,:,l);
-      %EmissComplete(:,:,i)=EmissComplete(:,:,i)+sinoAddEmiss(:,:,l);
-      MaskComplete (:,:,i)=MaskComplete (:,:,i)+sinoAddMask (:,:,l);
+      EmissComplete(:,:,i)=EmissComplete(:,:,i)+sinoAddEmiss(:,:,l);
+%      MaskComplete (:,:,i)=MaskComplete (:,:,i)+sinoAddMask (:,:,l);
     end
     l = l+1;
   end
@@ -742,28 +802,29 @@ for i=1:NSliTot
 end
 
 %% write Blank Complete to file
-name = strcat('SSRB_blank_nans_',filenameB,'.raw');
+name = strcat('SSRB_blank_noPost_',filenameB,'.raw');
 fid = fopen(name,'w');
 fwrite(fid,BlankComplete,'float32');
 fclose(fid);
 
 %% write Trans Complete to file
-name = strcat('SSRB_transSUB_nans_',filenameT,'.raw');
+name = strcat('SSRB_trans_noPost_',filenameT,'.raw');
 fid = fopen(name,'w');
 fwrite(fid,TransComplete,'float32');
 fclose(fid);
 
 %% write Mask Complete to file
-name = strcat('SSRB_Mask_',filenameT,'.raw');
-fid = fopen(name,'w');
-fwrite(fid,MaskComplete,'float32');
-fclose(fid);
+%name = strcat('SSRB_Mask_',filenameT,'.raw');
+%fid = fopen(name,'w');
+%fwrite(fid,MaskComplete,'float32');
+%fclose(fid);
 
 %% write Emiss Complete to file
-%name = strcat('SSRB_emiss_nans',filenameT,'.raw');
-%fid = fopen(name,'w');
-%fwrite(fid,EmissComplete,'float32');
-%fclose(fid);
+name = strcat('SSRB_emiss_noPost',filenameT,'.raw');
+fid = fopen(name,'w');
+fwrite(fid,EmissComplete,'float32');
+%fwrite(fid,sinoEm,'float32');
+fclose(fid);
 
 %% Calculate Ratio of Complete Blank and Transmission Scan
 BlankComplete = smooth3(BlankComplete,'gaussian',[3 3 3],0.42466);
@@ -771,24 +832,13 @@ TransComplete = smooth3(TransComplete,'gaussian',[3 3 3],0.42466);
 % medfilt3 was introduced in R2016b
 %BlankComplete = medfilt3(BlankComplete,[5 5 3]);
 %TransComplete = medfilt3(TransComplete,[5 5 3]);
-%EmissComplete = smooth3(EmissComplete,'gaussian',[3 3 3],0.42466);
+EmissComplete = smooth3(EmissComplete,'gaussian',[3 3 3],0.42466);
 %BlankComplete = smooth3(BlankComplete,'gaussian',[5 5 5],1);
 %TransComplete = smooth3(TransComplete,'gaussian',[5 5 5],1);
 %EmissComplete = smooth3(EmissComplete,'gaussian',[5 5 5],1);
 
-% correct for axial sensitivity of emission scan
-%for l = 1:NSliTot
-%  if l<8
-%  elseif l<64
-%    faktor = (l/10 + 3/10);
-%    EmissComplete(:,:,l) = EmissComplete(:,:,l)/faktor;
-%    EmissComplete(:,:,(127-l)) = EmissComplete(:,:,(127-l))/faktor;
-%    fprintf('Slice %i, faktor is: %f\r',l,faktor);
-%  end
-%end
-
 % Substrat emission sinogram from transmission scan
-%TransComplete = TransComplete - EmissComplete;
+TransComplete = TransComplete - EmissComplete;
 
 SinoRatio = zeros(Nbins,Nproj,NSliTot,'double');
 
@@ -808,7 +858,7 @@ for u=1:Nbins
     end
   end
 end
-name = strcat('SSRB_ratio_',filenameT,'.raw');
+name = strcat('SSRB_ratio_EmSub_noPost',filenameT,'.raw');
 fid = fopen(name,'w');
 fwrite(fid,SinoRatio,'float32');
 fclose(fid);
@@ -821,7 +871,9 @@ fclose(fid);
 %% Reconstruction of RatioComplete
 %filenameMask  = 'SSRB_997ratio_mask_600_04TransPhantom2HOT.raw';
 %filenameMask  = 'SSRB_mask_02TransPhantom2.raw';
-filenameMask  = 'SSRB_mask_03TransPhantom1.raw';
+%filenameMask  = 'SSRB_mask_03TransPhantom1.raw';
+filenameMask  = 'mask_transPig1.raw';
+%filenameMask  = 'mask_transPig2.raw';
 SinoRatio = FillSinoGaps(name,filenameMask);
 SinoRatio = smooth3(SinoRatio,'gaussian',[3 3 3],0.42466);
 %SinoRatio = smooth3(SinoRatio,'gaussian',[9 9 7],2);
@@ -831,8 +883,19 @@ fid = fopen(name,'w');
 fwrite(fid,SinoRatio,'float32');
 fclose(fid);
 
+% rectangular masking of ratio sinogram
+for i=1:Nbins
+  for j=1:Nproj
+    for k=1:NSliTot
+      if i<122 || i>222 || k<25
+        SinoRatio(i,j,k) = 0.0;
+      end
+    end
+  end
+end
+
 recon = 4.83559*OSEM_Recon(SinoRatio,filenameT);
-name = strcat('OSEM_ratio_SEG_smooth_',filenameT,'.raw');
+name = strcat('OSEM_ratio_SEG_',filenameT,'.raw');
 fid  = fopen(name, 'w');
 fwrite(fid,recon,'float32');
 fclose(fid);
